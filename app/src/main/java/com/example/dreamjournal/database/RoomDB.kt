01c286eda9954ed7109f7ba32e966d99844ee42e
fkorantengbarnes <fkorantengbarnes@ucvts.org>
2023-11-17 08:34:40 -0500

    things

--- app/src/main/java/com/example/dreamjournal/database/RoomDB.kt
+++ app/src/main/java/com/example/dreamjournal/database/RoomDB.kt
@@ -1,10 +1,36 @@
 package com.example.dreamjournal.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.dreamjournal.models.Log
+import java.lang.reflect.TypeVariable
+
+@Database(entities = [Log::class], version = 1, exportSchema = false)
+abstract class RoomDB : RoomDatabase() {
+
+    abstract fun mainDAO(): MainDAO
+    companion object {
+        private var database:RoomDB? = null
+        private var DATABASE_NAME = "asdfghjkl"
 
 
-class RoomDB {
-    @Database(entities = Log::class, version = 1, exportSchema = false)
+        @Synchronized
+        fun getInstance(context: Context): RoomDB {
+            if (database == null) {
+                database = Room.databaseBuilder(
+                    context.applicationContext,
+                    RoomDB::class.java,
+                    DATABASE_NAME
+                )
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+            return database!!
+        }
 
+
+    }
 }
