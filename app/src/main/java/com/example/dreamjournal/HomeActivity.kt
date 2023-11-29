d50c548ef85f61ebfafacdc2992e80e5fed68c87
fkorantengbarnes <fkorantengbarnes@ucvts.org>
2023-11-29 09:20:20 -0500

    thnings

--- app/src/main/java/com/example/dreamjournal/HomeActivity.kt
+++ app/src/main/java/com/example/dreamjournal/HomeActivity.kt
@@ -11,7 +11,8 @@ import com.example.dreamjournal.Adapters.LogsListAdapter
 import com.example.dreamjournal.database.RoomDB
 import com.example.dreamjournal.models.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
+import androidx.appcompat.widget.SearchView
+import java.util.Locale
 class HomeActivity : AppCompatActivity() {
 
     var recyclerView : RecyclerView ?= null
@@ -19,6 +20,7 @@ class HomeActivity : AppCompatActivity() {
     var logsList : List<Log> = ArrayList<Log>()
     var database : RoomDB  ?= null
     var fab_add : FloatingActionButton?= null
+    var searchView_home : SearchView?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +30,7 @@ class HomeActivity : AppCompatActivity() {
         fab_add = findViewById(R.id.fab_add)
         database = RoomDB.getInstance(this)
         logsList = database!!.mainDAO().getAll()
-
+        searchView_home = findViewById(R.id.searchView_home)
         updateRecycler(logsList)
 
         fab_add?.setOnClickListener {
@@ -36,6 +38,29 @@ class HomeActivity : AppCompatActivity() {
             {
                 val intent = Intent(this, LogsMakerActivity::class.java)
                 startActivity(intent)
+            }
+        }
+
+        searchView_home?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filter(newText?)
+                return true
+            }
+        })
+
+
+    }
+
+    private fun filter(newText: String) {
+        val filteredList : List<Log> = ArrayList<Log>()
+        for (log in logsList) {
+            if (log.title.lowercase().contains(newText.lowercase())
+            || log.content.lowercase().contains(newText.lowercase())) {
+                filteredList.add(log)
             }
         }
     }
--- app/src/main/java/com/example/dreamjournal/LogsMakerActivity.kt
+++ app/src/main/java/com/example/dreamjournal/LogsMakerActivity.kt
@@ -2,10 +2,37 @@ package com.example.dreamjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.example.dreamjournal.models.Log
 
 class LogsMakerActivity : AppCompatActivity() {
+    var editText_title : EditText ?= null
+    var editText_logs : EditText ?= null
+    var imageView_save : ImageView ?= null
+    var log : Log?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logs_maker)
+
+        imageView_save = findViewById(R.id.imageView_save)
+        editText_title = findViewById(R.id.editText_title)
+        editText_logs = findViewById(R.id.editText_logs)
+
+
+        imageView_save?.setOnClickListener({
+            fun onClick(view : View) {
+                val title : String = editText_title?.text?.toString()!!
+                val description : String = editText_logs?.text?.toString()!!
+
+                if (description.isEmpty()){
+                    Toast.makeText(this, "Please write about your dream", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        })
     }
 }
