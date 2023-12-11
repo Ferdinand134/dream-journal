870aecb1d721cbe0d1d8ecd1176c0c84acf1723f
fkorantengbarnes <fkorantengbarnes@ucvts.org>
2023-12-11 08:31:43 -0500

    we have a problem

--- app/src/main/java/com/example/dreamjournal/LogsMakerActivity.kt
+++ app/src/main/java/com/example/dreamjournal/LogsMakerActivity.kt
@@ -37,7 +37,7 @@ class LogsMakerActivity : AppCompatActivity() {
                 val formatter = SimpleDateFormat("EEE, dd MMM yyy HH:mm a", Locale.ENGLISH)
                 val date = Date()
 
-                log = Log()
+                /*log = Log()
 
                 log?.title = title
                 log?.content = description
@@ -45,6 +45,7 @@ class LogsMakerActivity : AppCompatActivity() {
 
                 val intent = Intent()
                 Intent.putExtra("log", log)
+                */
             }
         })
     }
